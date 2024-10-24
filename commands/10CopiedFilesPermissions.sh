7d10ae4ddde00cc877b03e59aa381a948a4123f6
Samuel Giroux <sgiroux@lauserco.com>
2024-10-24 16:18:35 -0400

    Edit resolv.conf file used from systemd-resolved service Add use of DNSSEC allow downgrade Add script to fix permissions of copied files from this repo

---
+++ commands/10CopiedFilesPermissions.sh
@@ -0,0 +1,4 @@
+chmod 644 /etc/systemd/network/*.network
+chmod 644 /etc/pacman.d/hooks/*.hook
+chmod 644 /etc/modprobe.d/*.conf
+chmod 644 /etc/vconsole.conf
--- commands/6NewCommandsPostBoot.sh
+++ commands/6NewCommandsPostBoot.sh
@@ -1,6 +1,9 @@
 # Configure DNS
 mv /etc/resolv.conf /etc/resolv.conf.bak
-ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+sudo mkdir /etc/systemd/resolved.conf.d
+echo '[Resolve]' | sudo tee /etc/systemd/resolved.conf.d/dnssec.conf
+echo 'DNSSEC=allow-downgrade' | sudo tee -a /etc/systemd/resolved.conf.d/dns.conf
 
 # Firmware file missing (SOF Intel)
 cd /usr/src
