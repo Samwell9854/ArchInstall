ef37155990aa5bacd2d24e1cfd9d7d8dd3247293
Samuel Giroux <sgiroux@lauserco.com>
2024-10-24 16:17:18 -0400

    Remove ZFSBootMenu edits that are no longer required Edit systemd sleep.conf to disable sleep and hibernation Comment in commands related to some services which the edits may no longer be needed Edit list of tools installed

--- commands/4CommandsInChrootPostUser.sh
+++ commands/4CommandsInChrootPostUser.sh
@@ -4,17 +4,20 @@ export ID
 mkdir /etc/pacman.d/hooks
 cp -r ../files/* /
 chown -R samuel:samuel /home/samuel/.config
-sed -i 's/  ManageImages: false/  ManageImages: true/' /etc/zfsbootmenu/config.yaml
-sed -i '6 a \ \ InitCPIO: true' /etc/zfsbootmenu/config.yaml
-sed -i 's/  Enabled: true/  Enabled: false/' /etc/zfsbootmenu/config.yaml
-sed -i ':a;N;$!ba;s/  Enabled: false/  Enabled: true/2' /etc/zfsbootmenu/config.yaml
 zfs set org.zfsbootmenu:commandline="rw quiet rd.vconsole.keymap=en net.ifnames=0 RESUME=none nvidia_drm.modeset=1" zroot/ROOT
 zed -F &
 sleep 3
 sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/*
 
-cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
-sed -i 's/MODULES=()/MODULES=(zfs)/' /etc/mkinitcpio.conf
+# No sleep at all
+sed -i 's/#AllowHibernation=.*/AllowHibernation=no/' /etc/systemd/sleep.conf
+sed -i 's/#AllowSuspendThenHibernate=.*/AllowSuspendThenHibernate=no/' /etc/systemd/sleep.conf
+sed -i 's/#AllowHybridSleep=.*/AllowHybridSleep=no/' /etc/systemd/sleep.conf
+sed -i 's/#AllowSuspend=.*/AllowSuspend=no/' /etc/systemd/sleep.conf
+
+sudo usermod -a -G power,lp,uucp,users,rfkill samuel
+
+sed -i.bak 's/MODULES=()/MODULES=(zfs)/' /etc/mkinitcpio.conf
 sed -i 's/HOOKS=(.*/HOOKS=\(base udev autodetect modconf keyboard block zfs filesystems\)/' /etc/mkinitcpio.conf
 # If issues, use this command instead:
 #sed -i 's/HOOKS=(.*/HOOKS=\(base udev autodetect modconf kms keyboard block zfs filesystems\)/' /etc/mkinitcpio.conf
@@ -29,9 +32,9 @@ systemctl enable systemd-timesyncd
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
 systemctl enable paccache.timer
-systemctl disable NetworkManager-wait-online.service
+#systemctl disable NetworkManager-wait-online.service
 systemctl mask fwupd.service
-systemctl mask systemd-udev-settle
+#systemctl mask systemd-udev-settle
 echo *** select man-db ***
-pacman --noconfirm -S dhcpcd dosfstools gptfdisk htop inetutils linux-firmware logrotate lsof man man-pages nmap openssh rsync screen smartmontools strace sysfsutils usbutils wget xfsprogs
+pacman --noconfirm -S btop dosfstools gptfdisk iftop inetutils iperf linux-firmware logrotate lsof nmap openssh rsync screen smartmontools strace sysfsutils usbutils wget xfsprogs
 echo Done in chroot, you may 'exit' back to live environment
